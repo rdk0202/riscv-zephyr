ccbcfe3e53d1349675361bf65542e73cfdacc12d
Nathaniel Graff <nathaniel.graff@sifive.com>
2018-10-22 15:49:39 -0700

    Frist blush at PMP configuration

--- soc/riscv32/riscv-privilege/common/pmp.c
+++ soc/riscv32/riscv-privilege/common/pmp.c
@@ -6,6 +6,7 @@
 
 #include <kernel.h>
 #include <soc.h>
+#include <arch/riscv32/riscv-privilege/pmp.h>
 
 /**
  * @brief Get the maximum number of partitions for a memory domain
@@ -22,7 +23,228 @@
  */
 int _arch_mem_domain_max_partitions_get(void)
 {
+	/* We use two regions per partition because we want to use top-of-region
+	 * (TOR) mode. */
+	return CONFIG_RISCV_PMP_NUM_ENTRIES / 2;
+}
+
+void set_pmpcfg(u8_t region, u32_t attr)
+{
+	u8_t cfg_num = region / 4;
+	u8_t subcfg_num = region % 4;
+
+	/* All regions are readable if configured and use TOR addressing */
+	u32_t cfg_value = RISCV_PRIV_PMPCFG_R | RISCV_PRIV_PMPCFG_A_TOR;
+
+	/* If the region is writeable, set the write bit */
 	/* TODO */
+
+	/* If the region is executable, set the execute bit */
+	/* TODO */
+
+	/* Shift the config to the proper pmpcfg CSR offset */
+	cfg_value = cfg_value << (sizeof(u8_t) * subcfg_num);
+	u32_t cfg_mask = 0xFF << (sizeof(u8_t) * subcfg_num);
+
+	/* Write the config to the proper mpmcfg0 */
+	u32_t pmpcfg;
+	switch(cfg_num)
+	{
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 0
+	case 0:
+		__asm__ volatile("csrr %[oldcfg], pmpcfg0"
+				: [oldcfg] "=r" (pmpcfg));
+		pmpcfg &= ~(cfg_mask);
+		pmpcfg |= cfg_value;
+		__asm__ volatile("csrw pmpcfg0, %[cfg]"
+				:: [cfg] "r" (pmpcfg));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES >= 5
+	case 1:
+		__asm__ volatile("csrr %[oldcfg], pmpcfg1"
+				: [oldcfg] "=r" (pmpcfg));
+		pmpcfg &= ~(cfg_mask);
+		pmpcfg |= cfg_value;
+		__asm__ volatile("csrw pmpcfg0, %[cfg]"
+				:: [cfg] "r" (pmpcfg));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES >= 9
+	case 2:
+		__asm__ volatile("csrr %[oldcfg], pmpcfg2"
+				: [oldcfg] "=r" (pmpcfg));
+		pmpcfg &= ~(cfg_mask);
+		pmpcfg |= cfg_value;
+		__asm__ volatile("csrw pmpcfg0, %[cfg]"
+				:: [cfg] "r" (pmpcfg));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES >= 13
+	case 3:
+		__asm__ volatile("csrr %[oldcfg], pmpcfg3"
+				: [oldcfg] "=r" (pmpcfg));
+		pmpcfg &= ~(cfg_mask);
+		pmpcfg |= cfg_value;
+		__asm__ volatile("csrw pmpcfg0, %[cfg]"
+				:: [cfg] "r" (pmpcfg));
+#endif
+	default:
+		return;
+	}
+}
+
+void clr_pmpcfg(u8_t region)
+{
+	u8_t cfg_num = region / 4;
+	u8_t subcfg_num = region % 4;
+
+	u32_t cfg_mask = 0xFF << (sizeof(u8_t) * subcfg_num);
+
+	/* Write the config to the proper mpmcfg0 */
+	u32_t pmpcfg;
+	switch(cfg_num)
+	{
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 0
+	case 0:
+		__asm__ volatile("csrr %[oldcfg], pmpcfg0"
+				: [oldcfg] "=r" (pmpcfg));
+		pmpcfg &= ~(cfg_mask);
+		__asm__ volatile("csrw pmpcfg0, %[cfg]"
+				:: [cfg] "r" (pmpcfg));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES >= 5
+	case 1:
+		__asm__ volatile("csrr %[oldcfg], pmpcfg1"
+				: [oldcfg] "=r" (pmpcfg));
+		pmpcfg &= ~(cfg_mask);
+		__asm__ volatile("csrw pmpcfg0, %[cfg]"
+				:: [cfg] "r" (pmpcfg));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES >= 9
+	case 2:
+		__asm__ volatile("csrr %[oldcfg], pmpcfg2"
+				: [oldcfg] "=r" (pmpcfg));
+		pmpcfg &= ~(cfg_mask);
+		__asm__ volatile("csrw pmpcfg0, %[cfg]"
+				:: [cfg] "r" (pmpcfg));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES >= 13
+	case 3:
+		__asm__ volatile("csrr %[oldcfg], pmpcfg3"
+				: [oldcfg] "=r" (pmpcfg));
+		pmpcfg &= ~(cfg_mask);
+		__asm__ volatile("csrw pmpcfg0, %[cfg]"
+				:: [cfg] "r" (pmpcfg));
+#endif
+	default:
+		return;
+	}
+}
+
+void set_pmpaddr(u8_t region, u32_t addr)
+{
+	switch(region)
+	{
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 0
+	case 0:
+		__asm__ volatile("csrw pmpaddr0, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 1
+	case 1:
+		__asm__ volatile("csrw pmpaddr1, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 2
+	case 2:
+		__asm__ volatile("csrw pmpaddr2, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 3
+	case 3:
+		__asm__ volatile("csrw pmpaddr3, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 4
+	case 4:
+		__asm__ volatile("csrw pmpaddr4, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 5
+	case 5:
+		__asm__ volatile("csrw pmpaddr5, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 6
+	case 6:
+		__asm__ volatile("csrw pmpaddr6, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 7
+	case 7:
+		__asm__ volatile("csrw pmpaddr7, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 8
+	case 8:
+		__asm__ volatile("csrw pmpaddr8, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 9
+	case 9:
+		__asm__ volatile("csrw pmpaddr9, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 10
+	case 10:
+		__asm__ volatile("csrw pmpaddr10, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 11
+	case 11:
+		__asm__ volatile("csrw pmpaddr11, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 12
+	case 12:
+		__asm__ volatile("csrw pmpaddr12, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 13
+	case 13:
+		__asm__ volatile("csrw pmpadd13, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 14
+	case 14:
+		__asm__ volatile("csrw pmpaddr14, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+#if CONFIG_RISCV_PMP_NUM_ENTRIES > 15
+	case 15:
+		__asm__ volatile("csrw pmpaddr15, %[addr]"
+				:: [addr] "r" (addr));
+#endif
+	default:
+		return;
+	}
+}
+
+int configure_pmp(u8_t partition, u32_t start, u32_t end, u32_t attr)
+{
+	if(partition >= _arch_mem_domain_max_partitions_get())
+		return 1;
+
+	u8_t lower_region = partition * 2;
+	u8_t upper_region = partition * 2 + 1;
+
+	/* Set lower bound */
+	set_pmpaddr(lower_region, start);
+
+	/* Set upper bound */
+	set_pmpaddr(upper_region, end);
+
+	/* Configure region */
+	set_pmpcfg(upper_region, attr);
+
 	return 0;
 }
 
@@ -38,7 +260,14 @@ int _arch_mem_domain_max_partitions_get(void)
  */
 void _arch_mem_domain_configure(struct k_thread *thread)
 {
-	/* TODO */
+	struct k_mem_domain *domain = thread->mem_domain_info.mem_domain;
+
+	for(int i = 0; i < domain->num_partitions; i++)
+	{
+		struct k_mem_partition part = domain->partitions[i];
+
+		configure_pmp(i, part.start, part.start + part.size, part.attr);
+	}
 }
 
 /**
@@ -54,7 +283,7 @@ void _arch_mem_domain_configure(struct k_thread *thread)
 void _arch_mem_domain_partition_remove(struct k_mem_domain *domain,
 		u32_t partition_id)
 {
-	/* TODO */
+	clr_pmpcfg(partition_id * 2 + 1);
 }
 
 /**
@@ -68,12 +297,15 @@ void _arch_mem_domain_partition_remove(struct k_mem_domain *domain,
  */
 void _arch_mem_domain_destroy(struct k_mem_domain *domain)
 {
-	/* TODO */
+	for(int i = 0; i < domain->num_partitions; i++)
+	{
+		clr_pmpcfg(i * 2 + 1);
+	}
 }
 
 int _arch_buffer_validate(void *addr, size_t size, int write)
 {
 	/* TODO */
-	return 1;
+	return 0;
 }
 
