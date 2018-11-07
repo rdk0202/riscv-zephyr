4694ece3d1a7b0e34b96c765f189d10234ad0799
Nathaniel Graff <nathaniel.graff@sifive.com>
2018-11-07 13:02:18 -0800

    Added sifive-sensors board for sensor testing

---
+++ boards/riscv32/sifive-sensors/board.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2017 Jean-Paul Etienne <fractalclone@gmail.com>
+ * Copyright (c) 2017 Palmer Dabbelt <palmer@dabbelt.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+/*
+ * UART clock configurations
+ *
+ * Define them here so that it can be replaced by global variables
+ * on other boards where the uart clock is determined dynamically
+ * following the PLL configuration
+ */
+#define uart_sifive_port_0_clk_freq    16000000
+#define uart_sifive_port_1_clk_freq    16000000
+#define i2c_sifive_port_0_clk_freq    16000000
+
+#define spi_sifive_port_0_clk_freq      16000000
+#define spi_sifive_port_1_clk_freq      16000000
+#define spi_sifive_port_2_clk_freq      16000000
+
+#define pwm_sifive_port_0_clk_freq  16000000
+#define pwm_sifive_port_1_clk_freq  16000000
+#define pwm_sifive_port_2_clk_freq  16000000
+
+#endif /* __INC_BOARD_H */
---
+++ boards/riscv32/sifive-sensors/pinmux.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2017 Jean-Paul Etienne <fractalclone@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <pinmux.h>
+#include <board.h>
+
+static int sifive_sensors_pinmux_init(struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	struct device *p = device_get_binding(CONFIG_PINMUX_SIFIVE_0_NAME);
+
+	/* UART0 RX */
+	pinmux_pin_set(p, 16, SIFIVE_PINMUX_IOF0);
+
+	/* UART0 TX */
+	pinmux_pin_set(p, 17, SIFIVE_PINMUX_IOF0);
+
+	/* SPI1 */
+	pinmux_pin_set(p, 3, SIFIVE_PINMUX_IOF0); /* MOSI */
+	pinmux_pin_set(p, 4, SIFIVE_PINMUX_IOF0); /* MISO */
+	pinmux_pin_set(p, 5, SIFIVE_PINMUX_IOF0); /* SCK */
+	pinmux_pin_set(p, 9, SIFIVE_PINMUX_IOF0); /* CS */
+
+	/* I2C 0 */
+	pinmux_pin_set(p, 12, SIFIVE_PINMUX_IOF0); /* SDA */
+	pinmux_pin_set(p, 13, SIFIVE_PINMUX_IOF0); /* SCL */
+
+	/* PWM 1 */
+	pinmux_pin_set(p, 19, SIFIVE_PINMUX_IOF1); /* PWM1_0 */
+	pinmux_pin_set(p, 20, SIFIVE_PINMUX_IOF1); /* PWM1_1 */
+	pinmux_pin_set(p, 21, SIFIVE_PINMUX_IOF1); /* PWM1_2 */
+	pinmux_pin_set(p, 22, SIFIVE_PINMUX_IOF1); /* PWM1_3 */
+
+	return 0;
+}
+
+SYS_INIT(sifive_sensors_pinmux_init, PRE_KERNEL_1, CONFIG_PINMUX_INIT_PRIORITY);
